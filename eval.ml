85ed42ad5f35a686b42204bc397ce0adc94c56c5
Chen Yukang <moorekang@gmail.com>
2015-06-02 17:52:48 +0800

    eval

---
+++ eval.ml
@@ -0,0 +1,14 @@
+open Type
+open Env
+       
+
+exception Runtime
+
+let rec eval exp env =
+  match exp with
+    Nil | Int _ -> exp
+    | Symbol {name = n} -> match (env_lookup env n).value with
+                             Value v -> v
+                           | _ -> raise Runtime
+    | _ -> Int 2
+               
